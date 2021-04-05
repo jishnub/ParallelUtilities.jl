c976db78389ee30266623ad62fafedf9dfc2ecdf
jishnub <jishnub@users.noreply.github.com>
2021-04-05 17:45:15 +0400

    hardlink instead of softlink in doc examples

--- docs/src/examples/pmapreduce.jl
+++ docs/src/examples/pmapreduce.jl
@@ -1,1 +1,38 @@
-../../../examples/pmapreduce.jl
+module PMapReduceTiming
+
+using ParallelUtilities
+using Distributed
+
+function initialize(sleeptime)
+    A = Array{Int}(undef, 20, 20)
+    for ind in eachindex(A)
+        sleep(sleeptime)
+        A[ind] = ind
+    end
+    return A
+end
+
+function main_mapreduce(sleeptime)
+    mapreduce(x -> initialize(sleeptime), hcat, 1:20)
+end
+
+function main_pmapreduce(sleeptime)
+    pmapreduce(x -> initialize(sleeptime), hcat, 1:20)
+end
+
+function compare_with_serial()
+    # precompile
+    main_mapreduce(0)
+    main_pmapreduce(0)
+
+    # time
+    println("Tesing serial")
+    A = @time main_mapreduce(5e-6)
+    println("Tesing parallel")
+    B = @time main_pmapreduce(5e-6)
+
+    # check results
+    println("Results match : ", A == B)
+end
+
+end
--- docs/src/examples/threads.jl
+++ docs/src/examples/threads.jl
@@ -1,1 +1,49 @@
-../../../examples/threads.jl
+module ThreadsTiming
+
+using ParallelUtilities
+using Distributed
+
+function initialize_serial(sleeptime)
+    s = zeros(Int, 2_000)
+    for ind in eachindex(s)
+        sleep(sleeptime)
+        s[ind] = ind
+    end
+    return s
+end
+
+function initializenode_threads(sleeptime)
+    s = zeros(Int, 2_000)
+    Threads.@threads for ind in eachindex(s)
+        sleep(sleeptime)
+        s[ind] = ind
+    end
+    return s
+end
+
+function main_threads(sleeptime)
+    workers_node_pool = ParallelUtilities.workerpool_nodes()
+    nw_nodes = nworkers(workers_node_pool)
+    pmapreduce(x -> initializenode_threads(sleeptime), hcat, workers_node_pool, 1:nw_nodes)
+end
+
+function main_serial(sleeptime)
+    workers_node_pool = ParallelUtilities.workerpool_nodes()
+    nw_nodes = nworkers(workers_node_pool)
+    mapreduce(x -> initialize_serial(sleeptime), hcat, 1:nw_nodes)
+end
+
+function compare_with_serial()
+    # precompile
+    main_serial(0)
+    main_threads(0)
+    # time
+    println("Testing serial")
+    A = @time main_serial(5e-3);
+    println("Testing threads")
+    B = @time main_threads(5e-3);
+
+    println("Results match : ", A == B)
+end
+
+end
